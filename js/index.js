76126706568285c122573721ba06f5d75b29370f
JaydenKimberlin <jayden.kimberlin1@gmail.com>
2018-03-27 11:13:34 -0400

    Smaller Update
    Social links, green dank sign

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // All Code Written and Provided by Jayden Kimberlin
+
+function socialOne() {
+    window.open('https://twitter.com/jaydenkimberlin');
+}
+
+function socialTwo() {
+    window.open('https://facebook.com/jdkphotog');
+}
