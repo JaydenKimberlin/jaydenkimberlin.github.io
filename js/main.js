71e1e5f9c061c6e9c648daa77d6447d58fc9ff1e
Jayden Kimberlin <jayden.kimberlin1@gmail.com>
2019-04-25 02:01:52 -0400

    Deletion
    New start

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('a[href*="#"]').on('click', function (e) {
-	e.preventDefault();
-
-	$('html, body').animate({
-		scrollTop: $($(this).attr('href')).offset().top - 80
-	}, 500, 'linear');
-});
-
-document.getElementById("write").innerHTML = "Last Modified: " + document.lastModified;
